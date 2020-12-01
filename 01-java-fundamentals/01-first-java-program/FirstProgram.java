6b483e5743c7cb16dd21c21bd221071c28bb4ddb
Hanna Smith <h.m.smith9595@gmail.com>
2020-12-01 17:57:11 -0500

    pythagorean/firstprogam

---
+++ 01-java-fundamentals/01-first-java-program/FirstProgram.java
@@ -0,0 +1,8 @@
+public class FirstProgram{
+    public static void main (String[] args){
+        System.out.println("My name is Hanna");
+    
+        System.out.println("I am 25 years old");
+        System.out.println("My hometown is Myrtle Beach, SC");
+    }
+}
---
+++ 01-java-fundamentals/02-pythagorean-theorem/Pythagorean.java
@@ -0,0 +1,6 @@
+public class Pythagorean{
+    public double calculateHypotenuse(int legA, int legB){
+        double hypotenuse = (Math.sqrt(legA*legA))+(Math.sqrt(legB*legB));
+            return hypotenuse;
+        }
+    }
