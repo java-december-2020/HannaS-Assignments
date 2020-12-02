00772b8aa41fa533bed451d2a3e3caa58a5f3429
Hanna Smith <h.m.smith9595@gmail.com>
2020-12-02 10:00:59 -0500

    pythagorean assignment

--- 01-java-fundamentals/02-pythagorean-theorem/Pythagorean.java
+++ 01-java-fundamentals/02-pythagorean-theorem/Pythagorean.java
@@ -1,6 +1,7 @@
-public class Pythagorean{
+public class Pythagorean {
+
     public double calculateHypotenuse(int legA, int legB){
         double hypotenuse = (Math.sqrt(legA*legA))+(Math.sqrt(legB*legB));
-            return hypotenuse;
+        return hypotenuse; 
         }
     }
