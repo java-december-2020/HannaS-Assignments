1045e8273b30e76ec56466a876b7aa0b6e89450c
Hanna Smith <h.m.smith9595@gmail.com>
2020-12-11 10:12:30 -0500

    object master pt 1

---
+++ 02-java-oop/05-object-master-pt1/HumanTest.java
@@ -0,0 +1,11 @@
+public class HumanTest{
+
+    public static void main(String[] args){
+
+    Human Hanna = new Human(); 
+    Human Alex = new Human();
+
+    Hanna.attack(Alex);
+        System.out.println(Alex.health);
+    }
+}
