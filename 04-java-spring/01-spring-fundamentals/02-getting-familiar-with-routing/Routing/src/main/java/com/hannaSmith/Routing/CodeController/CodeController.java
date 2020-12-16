e9a3cc439e9d9fd536d5a3bf53c79a7a2ce12abf
Hanna Smith <h.m.smith9595@gmail.com>
2020-12-16 09:35:31 -0500

    routing assignment

---
+++ 04-java-spring/01-spring-fundamentals/02-getting-familiar-with-routing/Routing/src/main/java/com/hannaSmith/Routing/CodeController/CodeController.java
@@ -0,0 +1,21 @@
+package com.hannaSmith.Routing.CodeController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/coding")
+public class CodeController {
+
+	public String coding() {
+		return "Hello Coding Dojo!";
+	}
+	@RequestMapping("/python")
+	public String python() {
+		return "Python/Django was awesome!";
+	}
+	@RequestMapping("/java")
+	public String java() {
+		return "Java/Spring is better!"; 
+	}
+}
